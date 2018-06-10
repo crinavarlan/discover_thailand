0fbf3a52689ed2e0558a44ecc65872b593ceacfd
Crina Varlan <crina.varlan@yahoo.com>
2018-06-10 12:39:47 +0100

    added some tests on about page

--- about/tests.py
+++ about/tests.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from about.views import about
+from django.urls import resolve
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_about_page(self):
+        about_page = resolve('/about/')
+        self.assertEqual(about_page.func, about)
+
+    def test_home_page_status_code_is_not_404(self):
+        home_page = self.client.get('/about/')
+        self.assertNotEquals(home_page.status_code, 404)
+
+    def test_home_page_status_code_is_not_500(self):
+        home_page = self.client.get('/about/')
+        self.assertNotEquals(home_page.status_code, 500)
