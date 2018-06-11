35b96aebfcfb6e1f383ea1ac56c87127fa7d8cc1
Crina Varlan <crina.varlan@yahoo.com>
2018-06-11 02:22:17 +0100

    tests for blog app added

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,24 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from .models import BlogPost
 from django.test import TestCase
+from blog.views import post_list
+from django.urls import resolve
 
-# Create your tests here.
+
+class BlogPostTests(TestCase):
+
+    def test_str(self):
+        test_title = BlogPost(title='My Latest Blog BlogPost')
+        self.assertEqual(str(test_title),
+                         'My Latest Blog BlogPost')
+
+    def test_about_page(self):
+            about_page = resolve('/blog/')
+            self.assertEqual(about_page.func, post_list)
+
+    def test_home_page_status_code_is_not_404(self):
+            home_page = self.client.get('/')
+            self.assertNotEquals(home_page.status_code, 404)
+
+    def test_home_page_status_code_is_not_500(self):
+            home_page = self.client.get('/')
+            self.assertNotEquals(home_page.status_code, 500)
