b6d2eaaaec36f4680de2162ba09c87058b60206c
Crina Varlan <crina.varlan@yahoo.com>
2018-06-11 02:02:17 +0100

    tests for contact page added

--- contact/tests.py
+++ contact/tests.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from contact.views import contact
+from django.urls import resolve
 
-# Create your tests here.
+
+class ContactPostTests(TestCase):
+
+    def test_about_page(self):
+            about_page = resolve('/contact/')
+            self.assertEqual(about_page.func, contact)
+
+    def test_home_page_status_code_is_not_404(self):
+            home_page = self.client.get('/')
+            self.assertNotEquals(home_page.status_code, 404)
+
+    def test_home_page_status_code_is_not_500(self):
+            home_page = self.client.get('/')
+            self.assertNotEquals(home_page.status_code, 500)
