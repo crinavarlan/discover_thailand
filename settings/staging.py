9314bf48a9746038815f71ce11e9d07aebd7d215
Crina Varlan <crina.varlan@yahoo.com>
2018-06-25 20:21:04 +0100

    changed to mysql database

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,18 @@
 from base import *
+import dj_database_url
+
 
 DEBUG = False
 
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Paypal environment variables
